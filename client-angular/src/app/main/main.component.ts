33abe2c806d17f113b2982e7a0c36eae0be10dc7
Evgeni Gordejev <evgeni.gordejev@igl.fi>
2024-03-23 10:12:40 +0200

    added angular template project

---
+++ client-angular/src/app/main/main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-component',
+  template: `<h1>Hello, World!</h1>`,
+  standalone: true
+})
+export class MainComponent { }
