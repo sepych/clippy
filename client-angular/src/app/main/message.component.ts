f4ae39685621464660d3c094eda1b53e7c93a203
Evgeni Gordejev <evgeni.gordejev@igl.fi>
2024-04-07 19:09:13 +0300

    moved svg files to assets folder

--- client-angular/src/app/main/message.component.ts
+++ client-angular/src/app/main/message.component.ts
@@ -4,6 +4,7 @@ import { SettingsModal } from './settings.modal';
 import { ServerMessage } from '../../../../common/types';
 import ColorLabelComponent from './color-label.component';
 import ColorWellComponent from './color-well.component';
+import SvgClipboard from '../../assets/clipboard';
 
 @Component({
   selector: 'message-component',
@@ -15,12 +16,7 @@ import ColorWellComponent from './color-well.component';
             {{ message.ipAddress }}
           </color-label>
           <button mat-button (click)="copyTextToClipboard(message.message)">
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-                 class="w-4 h-4">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M12 4C10.8954 4 10 4.89543 10 6H14C14 4.89543 13.1046 4 12 4ZM8.53513 4C9.22675 2.8044 10.5194 2 12 2C13.4806 2 14.7733 2.8044 15.4649 4H17C18.6569 4 20 5.34315 20 7V19C20 20.6569 18.6569 22 17 22H7C5.34315 22 4 20.6569 4 19V7C4 5.34315 5.34315 4 7 4H8.53513ZM8 6H7C6.44772 6 6 6.44772 6 7V19C6 19.5523 6.44772 20 7 20H17C17.5523 20 18 19.5523 18 19V7C18 6.44772 17.5523 6 17 6H16C16 7.10457 15.1046 8 14 8H10C8.89543 8 8 7.10457 8 6Z"
-                    fill="currentColor"></path>
-            </svg>
+            <svg-clipboard></svg-clipboard>
           </button>
         </div>
 
@@ -39,6 +35,7 @@ import ColorWellComponent from './color-well.component';
     MatButton,
     ColorLabelComponent,
     ColorWellComponent,
+    SvgClipboard,
   ],
   standalone: true,
 })
